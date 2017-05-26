97d946f4cb46553359aebabd4d39b7e64ab923ce
gradyasher <gradyasher@gmail.com>
2017-05-26 08:50:16 -0500

    yes

--- server.js
+++ server.js
@@ -112,6 +112,32 @@ app.delete('/recipes/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'id', 'ingredients']
+  for(let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i]
+    if(!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message)
+    }
+  }
+  if(req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`)
+    console.error(message)
+    return res.status(400).send()
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``)
+  Recipes.update({
+    name: req.body.name,
+    id: req.body.id,
+    ingredients: req.body.ingredients
+  })
+  res.status(204).end()
+})
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
